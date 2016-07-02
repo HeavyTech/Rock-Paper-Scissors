a1afb9db76d24029c04fefc91a77b6f9f740c536
Jose Virgen <Jose@HeroMacPro.local>
2016-07-02 16:56:37 -0700

    First commit

--- Rock Paper Scissors/ResultViewController.swift
+++ Rock Paper Scissors/ResultViewController.swift
@@ -8,28 +8,70 @@
 
 import UIKit
 
+enum Shape: String {
+    case Rock = "Rock"
+    case Paper = "Paper"
+    case Scissors = "Scissors"
+    
+    // This function randomly generates an opponent's play
+    static func randomShape() -> Shape {
+        let shapes = ["Rock", "Paper", "Scissors"]
+        let randomChoice = Int(arc4random_uniform(3))
+        return Shape(rawValue: shapes[randomChoice])!
+    }
+}
+
+
+
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    var userChoice:Shape!
+    private let opponentChoice: Shape = Shape.randomShape()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        displayResult()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    private func displayResult() {
+        // Ideally, most of this would be handled by a model.
+        var imageName: String
+        var text: String
+        let matchup = "\(userChoice.rawValue) vs. \(opponentChoice.rawValue)"
+        
+        // Why is an exclamation point necessary? :)
+        switch (userChoice!, opponentChoice) {
+        case let (user, opponent) where user == opponent:
+            text = "\(matchup): it's a tie!"
+            imageName = "tie"
+        case (.Rock, .Scissors), (.Paper, .Rock), (.Scissors, .Paper):
+            text = "You win with \(matchup)!"
+            imageName = "\(userChoice.rawValue)-\(opponentChoice.rawValue)"
+        default:
+            text = "You lose with \(matchup) :(."
+            imageName = "\(opponentChoice.rawValue)-\(userChoice.rawValue)"
+        }
+        
+        imageName = imageName.lowercaseString
+        resultImage.image = UIImage(named: imageName)
+        resultLabel.text = text
     }
-    */
 
+    
+
+    @IBAction func playAgain(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -20,6 +20,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRock(sender: UIButton) {
+        print("Rock was pressed")
+        
+        
+        performSegueWithIdentifier("play", sender: sender)
+        
+        
+//        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+//        controller.userChoice = getUserShape(sender)
+//        
+//        presentViewController(controller, animated: true, completion: nil)
+        
+        
 
+    }
+
+    @IBAction func playPaper(sender: UIButton) {
+        print("paper was pressed")
+        
+        performSegueWithIdentifier("play", sender: sender)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "play"{
+            let controller = segue.destinationViewController as! ResultViewController
+            controller.userChoice = getUserShape(sender as! UIButton)
+    }
+}
+    
+    
+    
+    private func getUserShape(sender: UIButton) -> Shape {
+        // Titles are set to one of: Rock, Paper, or Scissors
+        let shape = sender.titleForState(.Normal)!
+        return Shape(rawValue: shape)!
+    }
+
+    
 }
 
